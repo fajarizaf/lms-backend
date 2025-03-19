392b89152ed5faa5718c9547b26c425f2ebaaa0c
Fajar Riza Fauzi <fajarizaf@gmail.com>
2025-03-19 10:10:42 +0700

    update vercel 14.json

--- controllers/course-controller.js
+++ controllers/course-controller.js
@@ -12,17 +12,7 @@ export const createCourse = async (req, res) => {
 
 export const getAllCourses = async (req, res) => {
   try {
-    let query = {};
-    const { title, categoryId } = req.query;
-    if (title) {
-      query = { title: { $regex: title, $options: "i" } };
-    }
-    if (categoryId) {
-      query = { ...query, categoryId: categoryId };
-    }
-
-    const response = await courseModel.find(query);
-    res.status(200).json(response);
+    res.status(200).json({ "name": "hallo fajar" });
   } catch (error) {
     return res.status(401).json({ error, ms: "get all courses" });
   }
